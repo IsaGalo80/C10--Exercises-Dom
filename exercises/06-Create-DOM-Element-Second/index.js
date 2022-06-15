a902e78bf78ce10345a292c5399acb358c13b75b
IsaGalo80 <isagalo1980@outlook.com>
2022-06-15 14:31:21 +0000

    Solo falta el ultimo

--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,5 +1,4 @@
-let elem = document.createElement("#img");
-    image.src= "https://via.placeholder.com/350x150";
-    document.querySelector('#img').appendChild(image)
+document.body.innerHTML = `<h1>Hello World</h1><img src="http://placehold.it/350x150"/>`;
+
 
    
--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,2 +1,7 @@
 let myString = "<p>Hello!</p> <strong>My friend</strong>";
 document.write(myString);
+function getFullYear(){
+
+    let d = new Date("October 13, 2014 11:13:00");
+    document.getElementById("demo").innerHTML = d;
+};
